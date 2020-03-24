1dc5e61b512a61b5bd884165d13d81af66ef05be
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-24 15:43:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name, COUNT(*) FROM characters INNER JOIN character_books ON character_books.id = characters.id GROUP BY character_id ORDER BY COUNT(book_id) DESC"
+  "SELECT characters.name, COUNT(*) FROM characters INNER JOIN character_books ON character_books.id = characters.id GROUP BY character_id ORDER BY COUNT(book_id) DESC"
 end
